e31567d3a835e0f4fd17bf4ba86d09c9cc29e1ae
betti <jsy950601@gmail.com>
2020-01-07 22:22:15 +0900

    lombok
    -lombok 플러그인 install
    -exchange HelloController code to lombok
    
    *lombok 기능테스트에서 생성자 에러
    해결 - build.gradle에 아래의 의존성 추가
        testCompile "org.projectlombok:lombok"
        annotationProcessor('org.projectlombok:lombok')
        testAnnotationProcessor('org.projectlombok:lombok')

--- src/test/java/com/jsy/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/jsy/book/springboot/web/HelloControllerTest.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import  static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)  // 스프링 부트 테스트와 JUnit사이에 연결자 역할을 한다.
 @WebMvcTest
@@ -22,9 +25,24 @@ public class HelloControllerTest {
     public void hello_return() throws Exception{
         String hello = "hello";
 
-        mvc.perform(MockMvcRequestBuilders.get("/hello"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string(hello));
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
     }
+
+    @Test
+    public void helloDto_return() throws Exception {
+        String name = "hello";
+        int amount=1000;
+
+        mvc.perform(get("/hello/dto")
+                            .param("name",name)
+                            .param("amount",String.valueOf(amount)))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.name",is(name)))
+                                .andExpect(jsonPath("$.amount", is(amount)));
+
+    }
+
 
 }
