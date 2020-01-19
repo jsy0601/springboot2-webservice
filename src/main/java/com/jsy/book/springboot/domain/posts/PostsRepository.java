8af080f305215b607b6f6a71dbdf110d34c53815
betti <jsy950601@gmail.com>
2020-01-19 19:21:12 +0900

    oauth

--- src/main/java/com/jsy/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jsy/book/springboot/domain/posts/PostsRepository.java
@@ -2,6 +2,8 @@ package com.jsy.book.springboot.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -9,4 +11,6 @@ public interface PostsRepository extends JpaRepository<Posts,Long> {
 
     @Query(value = "SELECT * FROM POSTS p ORDER BY p.id DESC", nativeQuery = true)
     List<Posts> findAllDesc();
+
+
 }
--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -35,7 +35,10 @@ var main = {
         var _this = this;
     $('#btn-update').on('click',function(){
                 _this.update();
-            })
+            });
+    $('#btn-delete').on('click',function(){
+        _this.delete();
+    });
     },
     update : function(){
         var data = {
@@ -43,7 +46,6 @@ var main = {
                        author: $('#author').val(),
                        content: $('#content').val()
         };
-
         var id = $('#id').val();
 
         $.ajax({
@@ -58,7 +60,26 @@ var main = {
         }).fail(function(error){
             alert(JSON.stringify(error));
         });
+    },
+
+    delete : function(){
+     var id = $('#id').val();
+
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType: 'application/json; charset=utf-8'
+        }).done(function(){
+            alert('글이 삭제되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error){
+            alert(JSON.stringify(error));
+        });
+
     }
 };
 
 main.init();
+
